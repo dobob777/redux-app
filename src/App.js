20bed650147b7709b5a4bd70c31692c8684de2b0
dobob777 <dobob777@gmail.com>
2023-06-21 18:11:47 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logInData } from './actions/index'
 
-function App() {
+const App = () => {
+  const allDatas = useSelector((state) => {
+    return state.logInSaveData
+  });
+  const dispatch = useDispatch();
+  const [myData, setMyData] = useState([])
+
+  const [fData, setFData] = useState({
+    name: '',
+    email: '',
+  })
+  const sefData = (e) => {
+    setFData({ ...fData, [e.target.name]: e.target.value })
+  }
+
+  const saveData = () => {
+    // Validation
+    dispatch(logInData(fData))
+    // const ss = myData.push(fData);
+    // console.log('ss::: ', ss);
+    setMyData(myData => [...myData, fData]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <section style={ { width: "50%", marginTop: "50px" } }>
+          <article>
+            <div className="card">
+              <div>
+                <label htmlFor="name">Name</label>
+                <input
+                  name='name'
+                  value={ fData.name }
+                  onChange={ sefData }
+                  type="text"
+                  id='name' />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  name='email'
+                  value={ fData.email }
+                  onChange={ sefData }
+                  type="text"
+                  id='email' />
+              </div>
+              <button onClick={ saveData } style={ { marginTop: "20px" } }>Save</button>
+            </div>
+          </article>
+        </section>
+      </center>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            myData.map((d, index) => {
+              return (
+                <tr key={ index }>
+                  <td>{ d.name }</td>
+                  <td>{ d.email }</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
